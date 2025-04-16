9baefcf190f1db8a67b258c140410b8aa0af59a5
estefasilva <estefaniasilva510@hotmail.com>
2025-04-16 12:20:48 -0500

    actualización section educación, se agregan iconos al hero

--- script.js
+++ script.js
@@ -3,9 +3,15 @@ window.onload = function() {
   const items = document.querySelectorAll('.timeline-item');
   items.forEach(function(item, index) {
     setTimeout(function() {
-      item.classList.add('mostrar');
-    }, index * 300); // uno por uno
+      item.style.opacity = '1';
+      item.style.transform = 'translateY(0)';
+    }, index * 300);
   });
 };
+const toggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
 
+toggle.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
 
